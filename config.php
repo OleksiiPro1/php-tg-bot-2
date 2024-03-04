5d9c359924f900e175b4e4b359d3ceb2f391d4a3
Alexey Piltenko <101993475+OleksiiPro1@users.noreply.github.com>
2024-03-04 15:47:25 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,3 +1,3 @@
 <?php
-const TOKEN = '6553727559:AAFHWFRpdXftqNn2QMhyM54M4dz1GxMdlh4';
+const TOKEN = '65537127559:AAFHWFR4pdXftq3Nn2QMhyM524M4dz1GxMdlh4';
 const BASE_URL = 'https://api.telegram.org/bot' . TOKEN . '/';
